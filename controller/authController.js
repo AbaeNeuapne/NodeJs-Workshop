9cef2753d51038e67b6b0613a4a0bc1c805fa69e
Abae Neupane <108645361+AbaeNeuapne@users.noreply.github.com>
2024-09-10 13:22:21 +0545

    register and bcrypt defined

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,11 @@
 
-exports.renderRegister=(req,res)=>{
-    res.render("register")
-}
+const {registers} = require('c:/Users/abene/Desktop/node/model/index')
+const bcrypt= require('bcrypt')
 
 exports.registerUser= async (req,res)=>{
 
     const {username,email,password}= req.body
+    console.log(req.body)
     await registers.create({
         username,
         email,
@@ -14,42 +14,48 @@ exports.registerUser= async (req,res)=>{
     res.redirect("/login")
 }
 
-exports.renderLogin=(req,res)=>{
-    res.render("login")
-}
 
 exports.loginUser=async (req,res)=>{
     const {email,password} = req.body
     // check whether that email exist or not in users table 
     const data= await registers.findAll({
         where:{
-            email:email,
-            }    
-})
+            email:email
+        }    
+    })
     if(data.length==0){
         res.send("Email is not registered!!")
+    }
+    else{
+        const hashedPassword=data[0].password
+        const isValidPassword=bcrypt.compareSync(password,hashedPassword)
+        if(isValidPassword){
+            res.redirect("/")
         }
         else{
-            const hashedPassword=data[0].password
-            const isValidPassword=bcrypt.compareSync(password,hashedPassword)
-            if(isValidPassword){
-                res.redirect("/")
-                }
-                else{
-                    res.send("Invalid password!!")
-                    }
-             }
-
+            res.send("Invalid password!!")
+        }
+    }
+    
 //     if(data.length ==0){
-//         res.send("No user with that email")
-//     }else{
-//         // now check password 
-//        const isMatched =  bcrypt.compareSync(password,data[0].password)
-//        if(isMatched){
-//         res.send("Logged in success")
-//        }else{
-//         res.send("Invalid password")
-//        }
-//     }
-     
-}
+    //         res.send("No user with that email")
+    //     }else{
+        //         // now check password 
+        //        const isMatched =  bcrypt.compareSync(password,data[0].password)
+        //        if(isMatched){
+            //         res.send("Logged in success")
+            //        }else{
+                //         res.send("Invalid password")
+                //        }
+                //     }
+                
+            }
+            
+            
+            exports.renderRegister=(req,res)=>{
+                res.render("register")
+            }
+
+            exports.renderLogin=(req,res)=>{
+                res.render("login")
+            }
